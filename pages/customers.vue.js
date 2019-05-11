4c34f48fcf3b7835be150db6f50fad9fd3a4c3e0
Steven Woolston <steven.woolston@gmail.com>
2019-05-12 00:39:23 +1000

    Update customers.vue.js

--- pages/customers.vue.js
+++ pages/customers.vue.js
@@ -46,7 +46,7 @@ var spaCustomers = Vue.component("Customers", {
     methods: {
         getCustomers() {
             this.loading = true;
-            fetch(`${config.url}/customers`)
+            fetch(`${config.url}customers`)
                 .then(response => response.json())
                 .then((response) => {
                     this.customers = response.data;
