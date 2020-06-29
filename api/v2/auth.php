140711820a53a94f44bdf2718505e5c8650a927d
Steven Woolston <steven.woolston@gmail.com>
2020-06-29 23:52:02 +1000

    Removed namespace

--- api/v2/auth.php
+++ api/v2/auth.php
@@ -8,9 +8,9 @@ header("Access-Control-Allow-Methods: GET, HEAD, OPTIONS, POST, PUT, DELETE");
 require_once __DIR__ . '/../v2/config/database.php';
 require_once __DIR__ . '/../v2/objects/auth.php';
 
-$database = new APIv2\config\Database();
+$database = new Database();
 $db = $database->getConnection();
-$auth = new APIv2\objects\Auth($db);
+$auth = new Auth($db);
 $route = $_GET['route'];
 $data = json_decode(file_get_contents('php://input'), true);
 
