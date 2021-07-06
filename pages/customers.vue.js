aeb61f2c415beeaae46e8bacfb60dbc3bd1cad4e
Woolston Web Design <steven.woolston@gmail.com>
2021-07-06 22:18:19 +1000

    Update customers.vue.js

--- pages/customers.vue.js
+++ pages/customers.vue.js
@@ -9,45 +9,45 @@ var spaCustomers = Vue.component("Customers", {
         </div>
 
         <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Customer Name</th>
-                    <th>Last Note</th>
-                    <th style="width: 10%" class="text-center">Support Customer?</th>
-                    <th style="width: 10%" class="text-center">Active?</th>
-                </tr>
-            </thead>
-            <tbody v-if="customers.length > 0">
-                <tr v-for="customer in customers" :key="customer.Id">
-                    <td>
-                        <router-link :to="{name: 'Customer', params: {id: customer.Id, tabName: 'invoices'}}">{{customer.Name}}</router-link>
-                        <a style="margin-left: 20px;" class="text-primary"
-                            :href="customer.URL" v-if="customer.URL"
-                            target="_blank"><span class="glyphicon glyphicon-globe"></span>
-                        </a>
-                        <a style="margin-left: 10px;" class="text-primary"
-                            :href="customer.URL + '/wp-admin'" v-if="customer.URL"
-                            target="_blank"><span class="glyphicon glyphicon-cog"></span>
-                        </a>                        
-                    </td>
-                    <td>
-                        {{ customer.CreatedDate | moment }} <span v-bind:class="[customer.CreatedDate ? '' : 'hidden']">:</span> {{ customer.Description }}
-                    </td>
-                    <td class="text-center">
-                        <span class="glyphicon" v-bind:class="[customer.IsSupportCustomer ? 'text-success glyphicon-ok' : 'text-danger glyphicon-remove']"></span>
-                    </td>
-                    <td class="text-center">
-                        <span class="glyphicon" v-bind:class="[customer.IsVisible ? 'text-success glyphicon-ok' : 'text-danger glyphicon-remove']"></span>
-                    </td>
-                </tr>
-            </tbody>
-            <tbody v-else>
-                <tr>
-                    <td colspan="4" class="text-center">No matching records</td>
-                </tr>
-            </tbody>
-        </table>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Customer Name</th>
+                        <th>Last Note</th>
+                        <th style="width: 10%" class="text-center">Support Customer?</th>
+                        <th style="width: 10%" class="text-center">Active?</th>
+                    </tr>
+                </thead>
+                <tbody v-if="customers.length > 0">
+                    <tr v-for="customer in customers" :key="customer.Id">
+                        <td>
+                            <router-link :to="{name: 'Customer', params: {id: customer.Id, tabName: 'invoices'}}">{{customer.Name}}</router-link>
+                            <a style="margin-left: 20px;" class="text-primary"
+                                :href="customer.URL" v-if="customer.URL"
+                                target="_blank"><span class="glyphicon glyphicon-globe"></span>
+                            </a>
+                            <a style="margin-left: 10px;" class="text-primary"
+                                :href="customer.URL + '/wp-admin'" v-if="customer.URL"
+                                target="_blank"><span class="glyphicon glyphicon-cog"></span>
+                            </a>                        
+                        </td>
+                        <td>
+                            {{ customer.CreatedDate | moment }} <span v-bind:class="[customer.CreatedDate ? '' : 'hidden']">:</span> {{ customer.Description }}
+                        </td>
+                        <td class="text-center">
+                            <span class="glyphicon" v-bind:class="[customer.IsSupportCustomer ? 'text-success glyphicon-ok' : 'text-danger glyphicon-remove']"></span>
+                        </td>
+                        <td class="text-center">
+                            <span class="glyphicon" v-bind:class="[customer.IsVisible ? 'text-success glyphicon-ok' : 'text-danger glyphicon-remove']"></span>
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody v-else>
+                    <tr>
+                        <td colspan="4" class="text-center">No matching records</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
 
         <Loading :loading="this.loading"></Loading>
